afae6599424f1404d222504edecfcb58fe13110d
aMaxLow <maxim-pt@hotmail.com>
2018-05-08 11:51:36 +0400

    create data

---
+++ src/main/java/phonebook/BookData.java
@@ -0,0 +1,25 @@
+package phonebook;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class BookData {
+    private Map<String, ArrayList<String>> book = new HashMap<>();
+
+    BookData () {
+        addToMap("Иванов И.И." , "+8 800 2000 500" ,  "+8 800 200 600");
+        addToMap("Петров П.П.", "+8 800 2000 700");
+        addToMap("Сидоров С.С.", "+8 800 2000 800", "+8 800 2000 900 ", "+8 800 2000 000");
+
+    }
+
+    protected void  addToMap(String name, String... phoneNumber) {
+        ArrayList<String> phoneNumbers = new ArrayList<>();
+
+        Collections.addAll(phoneNumbers, phoneNumber);
+
+        book.put(name, phoneNumbers);
+    }
+}
